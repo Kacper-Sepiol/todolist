b9328c4be41eed95e6f8c584d4db2e07c2e4cac1
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2024-07-22 17:40:40 +0200

    the styles for Main.jsx have been improved, the Task.jsx components have been improved and styles have been created for it

--- src/components/Tasks/Task.jsx
+++ src/components/Tasks/Task.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { changeStatus } from "../../redux/actions";
 import { removeTask } from "../../redux/actions";
+import style from "./Task.module.css";
 
 export const Task = () => {
     const tasks = useSelector((state) => state.tasks);
@@ -19,24 +20,26 @@ export const Task = () => {
     return (
         <ul>
             {tasks.map((task) => (
-                <div key={task.id}>
-                    <h3>Name task: {task.nameTask}</h3>
-                    <p>
-                        completed
-                        <input
-                            type="checkbox"
-                            name="completedTask"
+                <div key={task.id} className={style.task}>
+                    <h3 className={style.task__text}>{task.nameTask}</h3>
+                    <div className={style.button__box}>
+                        <button
+                            type="button"
                             onClick={handleClickForm}
+                            className={style.buttons}
                             value={task.id}
-                        />
-                    </p>
-                    <button
-                        type="button"
-                        value={task.id}
-                        onClick={handleClickButton}
-                    >
-                        Usun
-                    </button>
+                        >
+                            completed
+                        </button>
+                        <button
+                            type="button"
+                            value={task.id}
+                            onClick={handleClickButton}
+                            className={style.buttons}
+                        >
+                            Usun
+                        </button>
+                    </div>
                 </div>
             ))}
         </ul>
